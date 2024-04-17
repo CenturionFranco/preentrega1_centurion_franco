1bd7b31c24256a06fd1bf674d73d14912b82a2ab
franco centurion <centurionfranco128@gmail.com>
2024-04-16 22:22:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+
+let estacion = prompt("Escribe una estación en minúsculas.");
+let opcion = prompt("Ingrese la opcion:");
+let saldo = 1000;
+
+do {
+
+
+}while(opcion === 0);
+
+switch (user){
+	case "Mateo":
+		document.write("Usted ha solicitado retirar efectivo. Cuanto quiere retirar?")
+		break;
+
+	case "Fernando":
+		alert("Se colorea el mundo.");
+		break;
+
+	case "verano":
+		alert("Las noches se hacen largas.");
+		break;
+
+	case "otoño":
+		alert("Donde había color ya no queda nada.");
+		break;
+
+	default:
+		alert("Eso no es una estación.");
+		break;
+}
